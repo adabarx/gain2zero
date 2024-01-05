898c3b396b53406c82b5e8dbd2aad3287c0110ff
katlyn-thomas <katlyn.c.thomas@gmail.com>
2024-01-05 13:41:52 -0600

    removed fluff

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,6 @@ use atomic_float::AtomicF32;
 
 mod editor;
 
-// This is a shortened version of the gain example with most comments removed, check out
-// https://github.com/robbert-vdh/nih-plug/blob/master/plugins/examples/gain/src/lib.rs to get
-// started
 
 struct GainToZero {
     params: Arc<GainToZeroParams>,
@@ -18,10 +15,6 @@ struct GainToZero {
 
 #[derive(Params)]
 struct GainToZeroParams {
-    /// The parameter's ID is used to identify the parameter in the wrappred plugin API. As long as
-    /// these IDs remain constant, you can rename and reorder these fields as you wish. The
-    /// parameters are exposed to the host in the same order they were defined. In this case, this
-    /// gain parameter is stored as linear gain while the values are displayed in decibels.
     #[persist = "editor-state"]
     editor_state: Arc<IcedState>,
 
@@ -57,9 +50,6 @@ impl Default for GainToZeroParams {
             )
                 .with_smoother(SmoothingStyle::Logarithmic(50.0))
                 .with_unit(" dB")
-                // There are many predefined formatters we can use here. If the gain was stored as
-                // decibels instead of as a linear gain value, we could have also used the
-                // `.with_step_size(0.1)` function to get internal rounding.
                 .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
                 .with_string_to_value(formatters::s2v_f32_gain_to_db()),
             reset: BoolParam::new("reset", false),
@@ -75,8 +65,6 @@ impl Plugin for GainToZero {
 
     const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-    // The first audio IO layout is used as the default. The other layouts may be selected either
-    // explicitly or automatically by the host or the user depending on the plugin API/backend.
     const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
         main_input_channels: NonZeroU32::new(2),
         main_output_channels: NonZeroU32::new(2),
@@ -84,9 +72,6 @@ impl Plugin for GainToZero {
         aux_input_ports: &[],
         aux_output_ports: &[],
 
-        // Individual ports and the layout as a whole can be named here. By default these names
-        // are generated as needed. This layout will be called 'Stereo', while a layout with
-        // only one input and output channel would be called 'Mono'.
         names: PortNames::const_default(),
     }];
 
@@ -96,13 +81,7 @@ impl Plugin for GainToZero {
 
     const SAMPLE_ACCURATE_AUTOMATION: bool = true;
 
-    // If the plugin can send or receive SysEx messages, it can define a type to wrap around those
-    // messages here. The type implements the `SysExMessage` trait, which allows conversion to and
-    // from plain byte buffers.
     type SysExMessage = ();
-    // More advanced plugins can use this to run expensive background tasks. See the field's
-    // documentation for more information. `()` means that the plugin does not have any background
-    // tasks.
     type BackgroundTask = ();
 
     fn params(&self) -> Arc<dyn Params> {
@@ -123,15 +102,7 @@ impl Plugin for GainToZero {
         _buffer_config: &BufferConfig,
         _context: &mut impl InitContext<Self>,
     ) -> bool {
-        // Resize buffers and perform other potentially expensive initialization operations here.
-        // The `reset()` function is always called right after this function. You can remove this
-        // function if you do not need it.
         true
-    }
-
-    fn reset(&mut self) {
-        // Reset buffers and envelopes here. This can be called from the audio thread and may not
-        // allocate. You can remove this function if you do not need it.
     }
 
     fn process(
@@ -146,8 +117,9 @@ impl Plugin for GainToZero {
 
             if !reset {
                 for sample in channel_samples {
-                    if sample.abs() * self.attenuation > threshold {
-                        self.attenuation = threshold / sample.abs();
+                    let sample_abs = sample.abs();
+                    if sample_abs * self.attenuation > threshold {
+                        self.attenuation = threshold / sample_abs;
                     }
                     *sample *= self.attenuation;
                 }
@@ -174,14 +146,12 @@ impl ClapPlugin for GainToZero {
     const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
     const CLAP_SUPPORT_URL: Option<&'static str> = None;
 
-    // Don't forget to change these features
     const CLAP_FEATURES: &'static [ClapFeature] = &[ClapFeature::Utility];
 }
 
 impl Vst3Plugin for GainToZero {
     const VST3_CLASS_ID: [u8; 16] = *b"Exactly16Chars!!";
 
-    // And also don't forget to change these categories
     const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
         &[Vst3SubCategory::Tools];
 }
